9314df8d272d174ebb2a7b5e1ae1e41b93b81a9c
20180663 <cd2541@kumoh.ac.kr>
2020-10-09 15:54:55 +0900

    Update 닉네임,시간 at App.vue, main.js, NoteEditor.vue

--- src/components/NoteEditor.vue
+++ src/components/NoteEditor.vue
@@ -2,6 +2,7 @@
     <div class="note-grid">
         <div class="note-editor">
             <input class="title-input" type="text" v-model="title" placeholder="Title">
+            <input class="title-input" type="text" v-model="nickname" placeholder="username">
             <textarea rows="10" v-model="text" placeholder="Take a note..."></textarea>            
             <div class="note-editor-bottom">
                 <button @click=createNew class="fas fas-check-circle"><i class="fas fa-check-circle"></i></button>
@@ -11,24 +12,31 @@
 </template>
 
 <script>
-
+import moment from 'moment'
 export default {
     data: function() {
         return {
+            nickname:'user',
             title: '',            
-            theme: '',            
-            text: ''
+            theme: '#F4CCCC',            
+            text: '',
+            time : moment().format('YYYY-MM-DD')
         }
     },   
     methods: {        
         createNew() {
-            this.$emit('noteAdded', this.title, this.text, this.theme);
+            this.$emit('noteAdded',this.nickname, this.title, this.text, this.theme,this.time);
+            this.nickname = 'user';
             this.title = '';
             this.text = '',
-            this.theme = '';
+            this.theme = '#F4CCCC';
+            this.time= moment().format('YYYY-MM-DD')   
         },
         deleteNote(index) {
             this.$emit('noteDeleted', index);
+        },
+        colorNote(index) {
+            this.$emit('noteColor', index);
         },
     },
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import Vue from 'vue'
+import moment from 'moment'
+import VueMomentJS from 'vue-momentjs'
+Vue.use(VueMomentJS, moment)
 import App from './App.vue'
 
 Vue.config.productionTip = false
