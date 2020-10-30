c0106b966213634eefbe28a24e5e92ba156cfcf4
dua9920 <dua9920@daum.net>
2020-10-30 14:35:07 +0900

    Upload tm file with npm install 실행 via 20180691

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
 import Vue from 'vue'
-import moment from 'moment'
-import VueMomentJS from 'vue-momentjs'
-Vue.use(VueMomentJS, moment)
 import App from './App.vue'
 
 Vue.config.productionTip = false
