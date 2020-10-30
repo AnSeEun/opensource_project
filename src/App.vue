b294888c5d9394831f495c019aeb0f2d8255cdbc
dua9920 <dua9920@daum.net>
2020-10-30 13:10:59 +0900

    Upload tf file via 20180691 npm 하기 전

--- src/App.vue
+++ src/App.vue
@@ -1,494 +1,88 @@
 <template>
   <div id="app">
-    <div class="header">
-      <img src="./assets/hellos_logo.png" />
-      <p><a href="javascript:location.reload()">Note Knock</a></p>
-      <p class="sub-title" style="font-size:70px; margin:0px">낰낰</p>
-    </div>
-    <div>
-      <select class="category-filter" v-model="selected">
-        <option value="">전체</option>
-        <option v-for="list in categorys" :key="list">
-          {{ list }}
-        </option>
-      </select>
-      <button
-        class="category-edit"
-        @click="modalCategory()"
-        @click.prevent="categoryMain = true"
-      >
-        카테고리 수정
-      </button>
-
-      <div v-if="categorylist">
-        <div>
-          <div class="category-modal-content">
-            <div
-              v-for="(list, index) in categorys"
-              :key="`list-${index}`"
-              class="category"
-            >
-              {{ list }}
-              <span v-if="index > 1" @click.prevent="deleteCategory(index)">
-                <i class="fas fa-times"></i>
-              </span>
+    <img id="image" src="./assets/dog.jpg">
+    <button @click=predict> Let's predict! </button>
+    <h1> Class: {{ predicted }} </h1>
+    <app-header @openEditor="editorOpen = !editorOpen"></app-header>
+    <app-note-editor v-if="editorOpen" @noteAdded="newNote" @noteDeleted="deleteNote"></app-note-editor>        
+    <div class="noteContainer">
+        <div v-for="(note, index) in notes" :key="`note-${index}`" class="note" :style="{'background-color': note.theme}">
+            <div>
+                <span class="delete" @click.prevent="deleteNote(index)"><i class="fas fa-times"></i></span> 
+                <span >{{ note.title}}</span>
+                <p class="note-text">{{ note.text }}</p>
             </div>
-            <categoryadd
-              :main="categoryMain"
-              @categoryAdd="addCategory"
-              @categoryDeleted="deleteCategory"
-              @categoryCancle="modalCategory"
-            ></categoryadd>
-          </div>
-          <div class="category-modal-layer"></div>
         </div>
-      </div>
     </div>
-
-    <div>
-      <SearchNote @noteSearched="searchNote"></SearchNote>
-    </div>
-
-    <table class="noteContainer">
-      <!--favorite note-->
-      <!-- <div class="favorite-part">Favorites</div> -->
-      <div class="note-favorite">
-        <tr
-          v-for="(note, index) in notesFilter(selected, search)"
-          v-show="note.favorite"
-          :key="`note-${index}`"
-          class="note"
-          :style="{ 'background-color': note.theme }"
-        >
-          <div>
-            <span class="favorites">
-              <!-- <i
-                class="far fa-star"
-                @click="addFavorite(index)"
-                v-if="!notes[index].favorite"
-              ></i> -->
-              <i class="fas fa-star" @click="deleteFavorite(index)"></i>
-            </span>
-            <span class="delete" @click.prevent="deleteNote(index)">
-              <i class="fas fa-times"></i>
-            </span>
-            <span class="note-date-span">
-              <font class="note-date" size="2em" color="#FFFFFF">
-                {{ note.time }}
-                <u>
-                  <i>By {{ note.nickname }}</i>
-                </u>
-              </font>
-            </span>
-            <input
-              class="title-view"
-              type="text"
-              v-model="note.title"
-              placeholder="Title"
-            />
-            <p />
-            <div v-if="note.category === 'To-do List'" id="checkbox">
-              <div v-for="index in note.listCount" :key="index">
-                <input
-                  type="checkbox"
-                  id="note.Todo[index-1]"
-                  value="note.Todo[index-1]"
-                  v-model="note.checked[index - 1]"
-                />
-                <label for="note.Todo[index-1]">
-                  <input
-                    class="todolist"
-                    type="text"
-                    v-model="note.Todo[index - 1]"
-                    placeholder="할 일"
-                  />
-                </label>
-              </div>
-              <i
-                class="fas fa-plus"
-                v-if="note.listCount < 5"
-                @click.prevent="note.listCount++"
-              >
-              </i>
-            </div>
-             <div v-else>
-          <textarea 
-            v-if="note.is_bold" 
-            style="font-weight:bold"
-            class="note-textarea"
-            rows="9"
-            v-model="note.text"
-            placeholder="Take a note..."
-          ></textarea>
-          <textarea 
-            v-else-if="note.is_under" 
-            style="text-decoration:underline"
-            class="note-textarea"
-            rows="9"
-            v-model="note.text"
-            placeholder="Take a note..."
-          ></textarea>
-          <textarea 
-            v-else-if="note.is_incli" style="fontStyle:italic"
-            class="note-textarea"
-            rows="9"
-            v-model="note.text"
-            placeholder="Take a note..."
-          ></textarea>
-          <textarea 
-            v-else
-            class="note-textarea"
-            rows="9"
-            v-model="note.text"
-            placeholder="Take a note..."
-          ></textarea>
-          </div>
-            <hr />
-            <span>
-              <span class="textform-B" @click="setBold(index)">B</span>
-              <span class="textform-U" @click="setUnderbar(index)">U</span>
-              <span class="textform-I" @click="setInclination(index)">I</span>
-            </span>
-            <span class="category-form">
-              <select v-model="note.category">
-                <option v-for="list in categorys" :key="list">
-                  {{ list }}
-                </option>
-              </select>
-            </span>
-
-            <span class="note-color" @click="modalColor(index)">
-              <i class="fas fa-palette"></i>
-            </span>
-            <div class="note-colorform" v-show="notes[index].is_show">
-              <ul>
-                <li class="color1" @click="setNoteColor(index, colors[0])"></li>
-                <li class="color2" @click="setNoteColor(index, colors[1])"></li>
-                <li class="color3" @click="setNoteColor(index, colors[2])"></li>
-                <li class="color4" @click="setNoteColor(index, colors[3])"></li>
-                <li class="color5" @click="setNoteColor(index, colors[4])"></li>
-              </ul>
-            </div>
-          </div>
-        </tr>
-      </div>
-
-      <hr class="part-line" />
-      <!--notes-->
-      <div class="note-part"></div>
-      <tr
-        v-for="(note, index) in notesFilter(selected, search)"
-        v-show="!note.favorite"
-        :key="`note-${index}`"
-        class="note"
-        :style="{ 'background-color': note.theme }"
-      >
-        <div>
-          <span class="favorites">
-            <i class="far fa-star" @click="addFavorite(index)"></i>
-          </span>
-          <span class="delete" @click.prevent="deleteNote(index)">
-            <i class="fas fa-times"></i>
-          </span>
-          <span class="note-date-span">
-            <font class="note-date" size="2em" color="#FFFFFF">
-              {{ note.time }}
-              <u>
-                <i>By {{ note.nickname }}</i>
-              </u>
-            </font>
-          </span>
-          <input
-            class="title-view"
-            type="text"
-            v-model="note.title"
-            placeholder="Title"
-          />
-          <p />
-          <div v-if="note.category === 'To-do List'" id="checkbox">
-            <div v-for="index in note.listCount" :key="index">
-              <input
-                type="checkbox"
-                id="note.Todo[index-1]"
-                value="note.Todo[index-1]"
-                v-model="note.checked[index - 1]"
-              />
-              <label for="note.Todo[index-1]">
-                <input
-                  class="todolist"
-                  type="text"
-                  v-model="note.Todo[index - 1]"
-                  placeholder="할 일"
-                />
-              </label>
-            </div>
-            <i
-              class="fas fa-plus"
-              v-if="note.listCount < 5"
-              @click.prevent="note.listCount++"
-            >
-            </i>
-          </div>
-          <div v-else>
-          <textarea 
-            v-if="note.is_bold" 
-            style="font-weight:bold"
-            class="note-textarea"
-            rows="9"
-            v-model="note.text"
-            placeholder="Take a note..."
-          ></textarea>
-          <textarea 
-            v-else-if="note.is_under" 
-            style="text-decoration:underline"
-            class="note-textarea"
-            rows="9"
-            v-model="note.text"
-            placeholder="Take a note..."
-          ></textarea>
-          <textarea 
-            v-else-if="note.is_incli" style="fontStyle:italic"
-            class="note-textarea"
-            rows="9"
-            v-model="note.text"
-            placeholder="Take a note..."
-          ></textarea>
-          <textarea 
-            v-else
-            class="note-textarea"
-            rows="9"
-            v-model="note.text"
-            placeholder="Take a note..."
-          ></textarea>
-          </div>
-          <hr />
-          <span>
-            <span class="textform-B" @click="setBold(index)">B</span>
-            <span class="textform-U" @click="setUnderbar(index)">U</span>
-            <span class="textform-I" @click="setInclination(index)">I</span>
-          </span>
-          <span class="category-form">
-            <select v-model="note.category">
-              <option v-for="list in categorys" :key="list">
-                {{ list }}
-              </option>
-            </select>
-          </span>
-
-          <span class="note-color" @click="modalColor(index)">
-            <i class="fas fa-palette"></i>
-          </span>
-          <div class="note-colorform" v-show="notes[index].is_show">
-            <ul>
-              <li class="color1" @click="setNoteColor(index, colors[0])"></li>
-              <li class="color2" @click="setNoteColor(index, colors[1])"></li>
-              <li class="color3" @click="setNoteColor(index, colors[2])"></li>
-              <li class="color4" @click="setNoteColor(index, colors[3])"></li>
-              <li class="color5" @click="setNoteColor(index, colors[4])"></li>
-            </ul>
-          </div>
-        </div>
-      </tr>
-
-      <app-note-editor
-        class="note-editor-container"
-        :categorylist="categorys"
-        v-if="editorOpen"
-        @noteAdded="newNote"
-        @noteDeleted="deleteNote"
-      >
-      </app-note-editor>
-
-      <button class="add-btn">
-        <i
-          id="plus"
-          class="fas fa-plus"
-          @click.prevent="editorOpen = !editorOpen"
-        ></i>
-      </button>
-    </table>
   </div>
 </template>
 
 <script>
-import NoteEditor from "./components/NoteEditor.vue";
-import NoteSearch from "./components/Search.vue";
-import categoryadd from "./components/CategoryAdd.vue";
+import NoteEditor from './components/NoteEditor.vue';
+import Header from './components/Header.vue';
+import * as cocoSSD from '@tensorflow-models/coco-ssd'
+import * as tf from '@tensorflow/tfjs'
+let model;
 
 export default {
-  name: "App",
+  name: 'App',
   data: function() {
     return {
       editorOpen: false,
-      selected: "",
-      search: "",
-      is_search: false,
-      categorylist: false,
-      categoryMain: false,
       notes: [
         {
-          category: "기본",
-          nickname: "user1",
-          title: "예시노트1",
-          text: "첫번째 노트입니다",
-          theme: "#F4CCCC",
-          time: "2020-10-25 Sun",
-          favorite: false,
-          is_show: false,
-          Todo: [],
-          checked: [],
-          listCount: 1,
-          is_bold: false,
-          is_under: false,
-          is_incli: false,
+          title: 'Code',
+          text: '1131111222',
+          theme: '#FF8A80',
         },
         {
-          category: "To-do List",
-          nickname: "user2",
-          title: "예시노트2",
-          text: "카테고리에 따라 바뀌는 내용입니다",
-          theme: "#FFF2CC",
-          time: "2020-10-25 Sun",
-          favorite: false,
-          is_show: false,
-          Todo: [],
-          checked: [],
-          listCount: 1,
-          is_bold: false,
-          is_under: false,
-          is_incli: false,
+          title: 'event',
+          text: 'event',
+          theme: '#DDA0DD',
         },
       ],
-      categorys: ["기본", "To-do List"],
-      colors: ["#F4CCCC", "#EB9F9F", "#E7D9E7", "#FFF2CC", "#F2F2F2"],
-      underbarCnt: 0,
-      inclinationCnt: 0,
-    };
+      predicted:"",      
+    }
   },
-
-  computed: {},
-
-  methods: {
-    newNote(
-      category,
-      nickname,
-      title,
-      text,
-      theme,
-      time,
-      favorite,
-      is_show,
-      Todo,
-      checked,
-      listCount,
-      is_bold,
-      is_under,
-      is_incli
-    ) {
-      this.notes.push({
-        category: category,
-        nickname: nickname,
-        title: title,
-        text: text,
-        theme: theme,
-        time: time,
-        favorite: favorite,
-        is_show: is_show,
-        Todo: Todo,
-        checked: checked,
-        listCount: listCount,
-        is_bold: is_bold,
-        is_under: is_under,
-        is_incli: is_incli,
-      });
-      this.editorOpen = false;
+	computed: {
+		
+	},
+  methods: {   
+    newNote(title, text, theme) {
+      this.notes.push({title: title, text: text, theme: theme});
     },
     deleteNote(index) {
-      this.notes.splice(index, 1);
+      this.notes.splice(index, 1)
     },
-    notesFilter: function(category, search) {
-      return this.notes.filter(function(note) {
-        return (
-          (note.category == category || category == "") &&
-          (note.text.includes(search) ||
-            note.title.includes(search) ||
-            search == "")
-        );
-      });
-    },
-    setNoteColor: function(index, color) {
-      this.notes[index].is_show = !this.notes[index].is_show;
-      this.notes[index].theme = color;
-    },
-    modalColor: function(index) {
-      this.notes[index].is_show = !this.notes[index].is_show;
-    },
-    searchNote(search) {
-      this.search = search;
-      this.is_search = true;
-    },
-    addCategory(category) {
-      this.categorys.push(category);
-    },
-    deleteCategory(index) {
-      this.categorys.splice(index, 1);
-    },
-    modalCategory: function() {
-      this.categorylist = !this.categorylist;
-    },
-    addFavorite: function(index) {
-      this.notes[index].favorite = true;
-      // console.log(index, this.notes[index].favorite_cnt);
-    },
-    deleteFavorite: function(index) {
-      this.notes[index].favorite = false;
-      // console.log(index, this.notes[index].favorite);
-    },
-    setBold: function(index) {
-      this.notes[index].is_bold = !this.notes[index].is_bold;
-
-      
-    },
-    setUnderbar: function(index) {
-      this.notes[index].is_under = !this.notes[index].is_under;
-    },
-    setInclination: function(index) {
-      this.notes[index].is_incli = !this.notes[index].is_incli;
-    },
-  },
-
-  mounted() {
-    if (localStorage.getItem("notes")){
-      this.notes = JSON.parse(localStorage.getItem("notes"));
+    async predict(){
+      const img = document.getElementById("image");       
+      let tmp = await model.detect(img);      
+      this.predicted = tmp[0].class
     }
-    if (localStorage.getItem("categorys"))
-      this.categorys = JSON.parse(localStorage.getItem("categorys"));
   },
-
+  async mounted() {
+    if (localStorage.getItem('notes')) this.notes = JSON.parse(localStorage.getItem('notes'));
+    model = await cocoSSD.load();
+    
+    console.log("model loaded");    
+     
+  },
   watch: {
     notes: {
       handler() {
         var newNotes = this.notes;
-        localStorage.setItem("notes", JSON.stringify(newNotes));
+        localStorage.setItem('notes', JSON.stringify(newNotes));
       },
       deep: true,
     },
-    categorys: {
-      handler() {
-        var addCategorys = this.categorys;
-        localStorage.setItem("categorys", JSON.stringify(addCategorys));
-      },
-    },
   },
-
   components: {
     appNoteEditor: NoteEditor,
-    SearchNote: NoteSearch,
-    categoryadd: categoryadd,
-  },
-};
+    appHeader: Header
+  }
+}
 </script>
 
 <style lang="scss">
-@import "/styles/global.scss";
+  @import '/styles/global.scss';
 </style>
--- src/components/NoteEditor.vue
+++ src/components/NoteEditor.vue
@@ -1,129 +1,35 @@
 <template>
-  <div class="note-grid">
-    <tr class="note-editor">
-      <input
-        class="title-input"
-        type="text"
-        v-model="title"
-        placeholder="Title"
-      />
-      <input
-        class="title-input-user"
-        type="text"
-        v-model="nickname"
-        placeholder="username"
-      />
-      <select v-model="category">
-        <option v-for="list in categorylist" :key="list">
-          {{ list }}
-        </option>
-        <option>사용자 추가</option>
-      </select>
-
-      <categoryadd
-        v-if="category === '사용자 추가'"
-        @categoryAdd="addCategory"
-      ></categoryadd>
-
-      <div v-if="category === 'To-do List'">
-        <div v-for="index in checkBoxAdd" :key="index">
-          <TodoList @listAdd="addList"></TodoList>
+    <div class="note-grid">
+        <div class="note-editor">
+            <input class="title-input" type="text" v-model="title" placeholder="Title">
+            <textarea rows="10" v-model="text" placeholder="Take a note..."></textarea>            
+            <div class="note-editor-bottom">
+                <button @click=createNew class="fas fas-check-circle"><i class="fas fa-check-circle"></i></button>
+            </div>
         </div>
-        <i
-          class="fas fa-plus"
-          v-if="checkBoxAdd < 5"
-          @click.prevent="checkBoxAdd++"
-        >
-        </i>
-      </div>
-
-      <textarea
-        v-else
-        rows="10"
-        v-model="text"
-        placeholder="Take a note..."
-      ></textarea>
-      <div class="note-editor-bottom">
-        <button @click="createNew" class="fas fas-check-circle">
-          <i class="fas fa-check-circle"></i>
-        </button>
-      </div>
-    </tr>
-  </div>
+    </div>
 </template>
 
 <script>
-import moment from "moment";
-import categoryadd from "./CategoryAdd.vue";
-import TodoList from "./TodoList.vue";
 
 export default {
-  data: function() {
-    return {
-      category: "기본",
-      nickname: "user",
-      title: "",
-      theme: "#F4CCCC",
-      text: "",
-      time: moment().format("YYYY-MM-DD ddd"),
-      favorite: false,
-      is_show: false,
-      checkBoxAdd: 0,
-      Todo: [],
-      TodoList: [],
-      is_bold: false,
-      is_under: false,
-      is_incli: false,
-    };
-  },
-  props: ["categorylist"],
-  methods: {
-    createNew() {
-      this.$emit(
-        "noteAdded",
-        this.category,
-        this.nickname,
-        this.title,
-        this.text,
-        this.theme,
-        this.time,
-        this.favorite,
-        this.is_show,
-        this.Todo,
-        this.TodoList,
-        this.checkBoxAdd,
-        this.is_bold,
-        this.is_under,
-        this.is_incli,
-      );
-      this.category = "기본";
-      this.nickname = "user";
-      this.title = "";
-      this.text = "";
-      this.theme = "#F4CCCC";
-      this.time = moment().format("YYYY-MM-DD ddd");
-      this.favorite = false;
-      this.is_show = false;
-      this.is_bold = false;
-      this.is_under = false;
-      this.is_incli = false;
+    data: function() {
+        return {
+            title: '',            
+            theme: '',            
+            text: ''
+        }
+    },   
+    methods: {        
+        createNew() {
+            this.$emit('noteAdded', this.title, this.text, this.theme);
+            this.title = '';
+            this.text = '',
+            this.theme = '';
+        },
+        deleteNote(index) {
+            this.$emit('noteDeleted', index);
+        },
     },
-    deleteNote(index) {
-      this.$emit("noteDeleted", index);
-    },
-    addCategory(category) {
-      this.categorylist.push(category);
-      this.category = category;
-      this.categorylist = !this.categorylist;
-    },
-    addList(todo, checked) {
-      this.Todo.push(todo);
-      this.TodoList.push(checked);
-    },
-  },
-  components: {
-    categoryadd: categoryadd,
-    TodoList: TodoList,
-  },
-};
+}
 </script>
